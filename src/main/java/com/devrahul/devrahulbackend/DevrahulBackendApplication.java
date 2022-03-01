b5612745e905ad7969ba7f6e9f0398800faf3fb0
Rahul Kumar <rahulcsofficial@gmail.com>
2022-03-02 00:31:42 +0530

    Initial Commit
    Signed-off-by: Rahul Kumar <rahulcsofficial@gmail.com>

---
+++ src/main/java/com/devrahul/devrahulbackend/DevrahulBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devrahul.devrahulbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevrahulBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevrahulBackendApplication.class, args);
+	}
+
+}
